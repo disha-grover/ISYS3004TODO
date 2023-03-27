efeec608cb70c8ff76ecc1c14496df47f8fac4d9
disha-grover <101254334+disha-grover@users.noreply.github.com>
2023-03-27 18:29:28 +0800

    updated the js

--- js/script.js
+++ js/script.js
@@ -4,28 +4,21 @@ var newTaskInput = document.getElementById("task-input");
 var todoListContainer = document.getElementById("todo-list");
 var templateContainer = document.getElementById("list-item-template");
 var template = templateContainer.innerHTML
+var showActiveButton = document.getElementById("show-active");
+var showAllButton = document.getElementById("show-all");
+var showCompletedButton = document.getElementById("show-completed");
 
 // Step 2 write the behaviour
+
 function onAddTaskClicked(event) {
     var taskName = newTaskInput.value;
     newTaskInput.value = "";
     var taskHTML = template.replace("<!--Task_Name-->", taskName);
     todoListContainer.insertAdjacentHTML('beforeend', taskHTML);
+    saveTasks(taskName, false);
 }
 
-function onTodolistClicked(event) {
-    let targetElement = event.target;
-    while (!targetElement.classList.contains("task")) {
-        targetElement = targetElement.parentElement;
-    }
-    let checkbox = targetElement.querySelector(".checkbox");
-    if (checkbox.checked) {
-        targetElement.classList.add("completed");
-    } else {
-        targetElement.classList.remove("completed");
-    }
-}
-/* function onTodoListContainerClicked(event) {
+function onTodoListContainerClicked(event) {
     var targetElement = event.toElement;
     
     while (!targetElement.classList.contains("task")){
@@ -38,8 +31,57 @@ function onTodolistClicked(event) {
         targetElement.classList.remove("completed");
     }
 }
-*/
+function showActiveTasks() {
+    var tasks = document.getElementsByClassName('task');
+    for (let i = 0; i < tasks.length; i++){
+        if (tasks[i].classList.contains("completed")){
+            tasks[i].style.display="none";
+        } else{
+            tasks[i].style.display="block";
+        }
+    }
+
+}
+
+function showAllTasks() {
+    var tasks = document.getElementsByClassName('task');
+    for (let i = 0; i < tasks.length; i++){
+        tasks[i].style.display="block";
+    }
+
+}
+
+function showCompletedTasks() {
+    var tasks = document.getElementsByClassName('task');
+    for (let i = 0; i < tasks.length; i++){
+        if (tasks[i].classList.contains("completed")){
+            tasks[i].style.display="block";
+        } else{
+            tasks[i].style.display="none";
+        }
+    }
+
+}
+
+function renderTasks(){
+    for (i = 0; i< localStorage.length; i++){
+        var taskName = localStorage.key(i)
+        var isCompleted = localStorage.getItem(taskName) == "true";
+        var taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
+        if(!isCompleted){
+            todoListContainer.insertAdjacentHTML('afterbegin', taskHTML);
+        }
+    }
+}
+
+function saveTasks(name, isCompleted){
+    localStorage.setItem(name, isCompleted);
 
 // Step 3 link to event handler 
 addTaskButton.addEventListener('click', onAddTaskClicked);
-todoListContainer.addEventListener('click', onTodoListContainerClicked); 
+todoListContainer.addEventListener('click', onTodoListContainerClicked);
+showActiveButton.addEventListener('click', showActiveTasks);
+showAllButton.addEventListener('click', showAllTasks);
+showCompletedButton.addEventListener('click', showCompletedTasks);
+
+renderTasks() 
