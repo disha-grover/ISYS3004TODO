6c8e26494d70cccd05e9728dd67b2ee6615480ec
disha-grover <101254334+disha-grover@users.noreply.github.com>
2023-03-27 15:49:50 +0800

    Uploaded the css and js file made in Visual Studio

---
+++ script.js
@@ -0,0 +1,32 @@
+// Step 1 find the element
+var addTaskButton = document.getElementById("add-task");
+var newTaskInput = document.getElementById("task-input");
+var todoListContainer = document.getElementById("todo-list");
+var templateContainer = document.getElementById("list-item-template");
+var template = templateContainer.innerHTML
+
+// Step 2 write the behaviour
+function onAddTaskClicked(event) {
+    var taskName = newTaskInput.value;
+    newTaskInput.value = "";
+    var taskHTML = template.replace("<!--Task_Name-->", taskName);
+    todoListContainer.insertAdjacentHTML('afterbegin', taskHTML);
+}
+
+function onTodoListContainerClicked(event) {
+    var targetElement = event.toElement;
+    
+    while (!targetElement.classList.contains("task")){
+        targetElement = targetElement.parentElement; 
+    }
+    var checkbox = targetElement.querySelector(".checkbox");
+    if (checkbox.checked){
+        targetElement.classList.add("completed")
+    } else {
+        targetElement.classList.remove("completed")
+    }
+}
+
+// Step 3 link to event handler 
+addTaskButton.addEventListener('click', onAddTaskClicked);
+todoListContainer.addEventListener('click', onTodoListContainerClicked); 
